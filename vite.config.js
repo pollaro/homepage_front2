b1d6aac3de16d0ca1886fd6233956503c1afc094
Jim <jim.pollaro@gmail.com>
2023-05-09 14:03:08 -0700

    fix vite proxy

--- vite.config.js
+++ vite.config.js
@@ -15,10 +15,22 @@ export default defineConfig(() => {
     envDir: '.',
     server: {
       proxy: {
-        '^/api/.*': {
+        '/api': {
           target: 'https://127.0.0.1:8000',
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, '')
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          configure: (proxy, _options) => {
+            proxy.on('error', (err, _req, _res) => {
+              console.log('proxy error', err)
+            })
+            proxy.on('proxyReq', (proxyReq, req, _res) => {
+              console.log('Sending Request to the Target:', req.method, req.url)
+            })
+            proxy.on('proxyRes', (proxyRes, req, _res) => {
+              console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+            })
+          }
         }
       }
     }
