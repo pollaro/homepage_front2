0d0e8e102deb9350e83d1a3aca6f5b8b6face38e
Jim <jim.pollaro@gmail.com>
2023-05-18 10:28:11 -0700

    change config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig(() => {
       }
     },
     envDir: '.',
-    server: {
+    devServer: {
       proxy: {
         '/api': {
           target: 'https://127.0.0.1:8000',
